a3caee0c2fe467502a89c80438eb6bdb6787b852
dtsoi <tsoi.daniela@gmail.com>
2018-01-19 10:19:05 -0500

    Cleaned up code and made successful calls to API
    Changed searchButtons() into an  anonymous function for cleaner code.
    
    Slide toggle will empty search values.
    
    Toggle and closeOverlay functions in said anonymous function.
    
    Closing overlay will now empty results' html codes.

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const planetsURL = "https://swapi.co/api/planets/?search=";
 const speciesURL = "https://swapi.co/api/species/?search=";
 const starshipersURL = "https://swapi.co/api/starshipers/?search=";
 const vehiclesURL = "https://swapi.co/api/vehicles/?search=";
-let word = '';
 
 //Call YouTube API
 function getDataVideo(searchWord, callback) {
@@ -14,7 +13,7 @@ function getDataVideo(searchWord, callback) {
     channelId: 'UCZGYJFUizSax-yElQaFDp5Q',
     key: 'AIzaSyBWlaqRf5RJqSa4OqlSqdjdr_GaYUmpcuY',
     q: `${searchWord}`,
-    maxResults: 3,
+    maxResults: 3
   }
   $.getJSON(videoURL, settings, callback);
 }
@@ -23,117 +22,106 @@ function getDataVideo(searchWord, callback) {
 function getSWData(url, query, callback) {
     $.ajax({
         method: "GET",
-        url: url
+        url: url + query
     }).done(callback);
 };
 
-function searchButtons() {
+(function() {
+    $('.submit-input.films').on('click', function() {
+        let word = $('.search-item.films').val();
+        getSWData(filmsURL, word, handleFilmsData);
+        //getDataVideo(word, displayVideoResults);
+    });
+    
+    $('.submit-input.people').on('click', function() {
+        let word = $('.search-item.people').val();
+        getSWData(peopleURL, word, handlePeopleData);
+        //getDataVideo(word, displayVideoResults);
+    });
 
-    function filmsSearchButton() {
-        $('.topic films').on('click', function() {
-            getSWData(filmsURL, word, handleFilmsData);
-        });
-    }
+    $('.submit-input.planets').on('click', function() {
+        let word = $('.search-item.planets').val();
+        getSWData(peopleURL, word, handlePlanetsData);
+        //getDataVideo(word, displayVideoResults);
+    });
 
-    function peopleSearchButton() {
-        $('.topic people').on('click', function() {
-            getSWData(peopleURL, '', handlePeopleData);
-        });
-    }
+    $('.submit-input.species').on('click', function() {
+        let word = $('.search-item.species').val();
+        getSWData(peopleURL, word, handleSpeciesData);
+        //getDataVideo(word, displayVideoResults);
+    });
 
-    function planetsSearchButton() {
-        $('.topic planets').on('click', function() {
-            getSWData(planetsURL, '',handlePlanetsData);
+    $('.submit-input.species').on('click', function() {
+        let word = $('.search-item.species').val();
+        getSWData(peopleURL, word, handleSpeciesData);
+        //getDataVideo(word, displayVideoResults);
+    });
+    
+    //Close overlay and empty html results
+        $('.overlay').on('click', function() {
+            $(this).removeClass('active');
+            $('.swapi-results').empty();
+            $('.youtube-results').empty();
         });
-    }
 
-    function speciesSearchButton() {
-        $('.topic species').on('click', function() {
-            getSWData(speciesURL, '',handleSpeciesData);
+    //Toggle search bar under buttons
+        $('body').on('click', '.topic', function(event) {
+            let slide = $(this).parent().find('.search-line');
+            $('.search-line').not(slide).slideUp();
+            $('.search-item').val('');
+            slide.slideToggle();
         });
-    }
 
-    function starshipsSearchButton() {
-        $('.topic starships').on('click', function() {
-            getSWData(starshipsURL, '',handleStarshipsData);
-        });
-    }
-
-    function vehiclesSearchButton() {
-        $('.topic vehicles').on('click', function() {
-            getSWData(vehiclesURL, '',handleVehiclesData);
-        });
-    }
-}
+})();
 
 function handleFilmsData(data) {
-    $('.search-results').html(data);
+    //$('.overlay').addClass('active');
+    console.log(data);
+    $('.swapi-results').html(`
+        title: ${data.results[0].title}
+        director: ${data.results[0].director}
+    `);
 }
 
-//Toggle search bar under buttons
-function searchToggle() {
-    $('body').on('click', '.topic', function(event) {
-        let slide = $(this).parent().find('.search-line');
-        $('.search-line').not(slide).slideUp();
-        slide.slideToggle();
-    });
+function handlePeopleData(data) {
+    //$('.overlay').addClass('active');
+    console.log(data);
+    $('.swapi-results').html(`
+        name: ${data.results[0].name}
+    `);
+}
+
+function handlePlanetsData(data) {
+    //$('.overlay').addClass('active');
+    console.log(data);
+}
+
+function handleSpeciesData(data) {
+    //$('.overlay').addClass('active');
+    console.log(data);
+}
+
+function handleStarshipsData(data) {
+    //$('.overlay').addClass('active');
+    console.log(data);
+}
+
+function handleVehiclesData(data) {
+    //$('.overlay').addClass('active');
+    console.log(data);
 }
 
 //Blank space code for results
-function blankResults(result) {
+function blankVideoResults(result) {
   return `
   <br>
     <div>
-    <iframe id="youtube" width="960" height="540" src="https://www.youtube.com/embed/${result.id.videoId}?showinfo=0" frameborder="0" allowfullscreen></iframe>
+    <iframe id="youtube" width="400" height="225" src="https://www.youtube.com/embed/${result.id.videoId}?showinfo=0" frameborder="0" allowfullscreen></iframe>
     </div>`;
 }
 
-//Insert data results into blank space code
-function displayResults(data) {
-  const videos = data.items.map((item,index) => blankResults(item));
-  $('.search-results').html(videos);
+//Insert YT data results into blank space code
+function displayVideoResults(data) {
+  const videos = data.items.map((item,index) => blankVideoResults(item));
+  $('.youtube-results').html(videos);
 }
-
-function closeBtn () {
-  $('.close-btn').on('click', function () {
-    $(this).removeClass('active');
-  });
-}
-
-function openOverlay() {
-    $('.overlay').on('click', function() {
-        $(this).addClass('active');
-    });
-}
-
-function closeOverlay() {
-  $('.overlay').on('click', function() {
-    $(this).removeClass('active');
-  });
-}
-
-function submitButton() {
-  $('.submit-input').submit(event => {
-    openOverlay();
-    //$('.overlay').toggleClass();
-    event.preventDefault();
-    const searchItem = $(event.currentTarget).find('.search-item');
-    //const searchTopic = $().val();
-
-    const word = searchItem.val();
-    getDataVideo(word, displayResults);
-    });
-}
-
-function handleFunctions() {
-  submitButton();
-  closeOverlay();
-  closeBtn();
-  searchToggle();
-  searchButtons();
-  handleFilmsData();
-}
-
-handleFunctions();
-
-//<iframe id="youtube" width="960" height="540" src="https://www.youtube.com/embed/${result.id.videoId}?showinfo=0" frameborder="0" allowfullscreen></iframe>
