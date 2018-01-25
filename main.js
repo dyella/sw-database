273389ab9c28f6a780e18c4aaea629d58e26cd1c
dtsoi <32502046+dyella@users.noreply.github.com>
2018-01-25 00:40:20 -0500

    Repo one (#1)
    * First repo
    
    Added splash page and instructions
    
    Added data calls for all categories
    
    Need to implement Google Search API and style html in overlay
    
    * Delete index.css
    
    * Delete index.js

--- main.js
+++ main.js
@@ -5,58 +5,56 @@ const planetsURL = "https://swapi.co/api/planets/?search=";
 const speciesURL = "https://swapi.co/api/species/?search=";
 const starshipersURL = "https://swapi.co/api/starshipers/?search=";
 const vehiclesURL = "https://swapi.co/api/vehicles/?search=";
+const imgURL = "https://www.googleapis.com/customsearch/v1";
 
-//Call YouTube API
-function getDataVideo(searchWord, callback) {
-  const settings = {
-    part: 'snippet',
-    channelId: 'UCZGYJFUizSax-yElQaFDp5Q',
-    key: 'AIzaSyBWlaqRf5RJqSa4OqlSqdjdr_GaYUmpcuY',
-    q: `${searchWord}`,
-    maxResults: 3
-  }
-  $.getJSON(videoURL, settings, callback);
+function startOp() {
+    $('.splash').on('click', function(event) {
+        $('.splash').fadeOut(500, function() {
+            $('.splash').addClass('hidden');
+            $('.navigate').fadeIn(500);
+            $('.navigate').removeClass('hidden');
+        });
+    });
 }
 
-//Call Star Wars API
-function getSWData(url, query, callback) {
-    $.ajax({
-        method: "GET",
-        url: url + query
-    }).done(callback);
-};
 
 (function() {
     $('.submit-input.films').on('click', function() {
         let word = $('.search-item.films').val();
         getSWData(filmsURL, word, handleFilmsData);
-        //getDataVideo(word, displayVideoResults);
+        getDataVideo(word, displayVideoResults);
     });
     
     $('.submit-input.people').on('click', function() {
         let word = $('.search-item.people').val();
         getSWData(peopleURL, word, handlePeopleData);
-        //getDataVideo(word, displayVideoResults);
+        getDataVideo(word, displayVideoResults);
     });
 
     $('.submit-input.planets').on('click', function() {
         let word = $('.search-item.planets').val();
-        getSWData(peopleURL, word, handlePlanetsData);
-        //getDataVideo(word, displayVideoResults);
+        getSWData(planetsURL, word, handlePlanetsData);
+        getDataVideo(word, displayVideoResults);
     });
 
     $('.submit-input.species').on('click', function() {
         let word = $('.search-item.species').val();
-        getSWData(peopleURL, word, handleSpeciesData);
-        //getDataVideo(word, displayVideoResults);
+        getSWData(speciesURL, word, handleSpeciesData);
+        getDataVideo(word, displayVideoResults);
     });
 
-    $('.submit-input.species').on('click', function() {
-        let word = $('.search-item.species').val();
-        getSWData(peopleURL, word, handleSpeciesData);
-        //getDataVideo(word, displayVideoResults);
+    $('.submit-input.starships').on('click', function() {
+        let word = $('.search-item.starships').val();
+        getSWData(starshipsURL, word, handleStarshipsData);
+        getDataVideo(word, displayVideoResults);
     });
     
+    $('.submit-input.vehicles').on('click', function() {
+        let word = $('.search-item.vehicles').val();
+        getSWData(vehiclesURL, word, handleVehiclesData);
+        getDataVideo(word, displayVideoResults);
+    });
+
     //Close overlay and empty html results
         $('.overlay').on('click', function() {
             $(this).removeClass('active');
@@ -72,43 +70,104 @@ function getSWData(url, query, callback) {
             slide.slideToggle();
         });
 
+    startOp();
+
 })();
 
+//Call YouTube API
+function getDataVideo(searchWord, callback) {
+  const settings = {
+    part: 'snippet',
+    channelId: 'UCZGYJFUizSax-yElQaFDp5Q',
+    key: 'AIzaSyBWlaqRf5RJqSa4OqlSqdjdr_GaYUmpcuY',
+    q: `${searchWord}`,
+    maxResults: 5
+  }
+  $.getJSON(videoURL, settings, callback);
+}
+
+//Call Star Wars API
+function getSWData(url, query, callback) {
+    $.ajax({
+        method: "GET",
+        url: url + query
+    }).done(callback);
+};
+
+//Call Google Search API
+function getIMGData(url, query, callback) {
+    $.ajax({
+        method: "GET",
+        url: url + query
+    }).done(callback);
+};
+// API key "AIzaSyB4g8c1vSsEIfHvEMCeWuFzvCIQTfGJdPc"
+
 function handleFilmsData(data) {
-    //$('.overlay').addClass('active');
+    $('.overlay').addClass('active');
     console.log(data);
     $('.swapi-results').html(`
         title: ${data.results[0].title}
+        episode: ${data.results[0].episode_id}
         director: ${data.results[0].director}
+        opening: ${data.results[0].opening_crawl}
     `);
 }
 
 function handlePeopleData(data) {
-    //$('.overlay').addClass('active');
+    $('.overlay').addClass('active');
     console.log(data);
     $('.swapi-results').html(`
         name: ${data.results[0].name}
+        birth year: ${data.results[0].birth_year}
+        home planet: ${data.results[0].homeworld}
+        species: ${data.results[0].species}
     `);
 }
 
 function handlePlanetsData(data) {
-    //$('.overlay').addClass('active');
+    $('.overlay').addClass('active');
     console.log(data);
+    $('.swapi-results').html(`
+        name: ${data.results[0].name}
+        climate: ${data.results[0].climate}
+        population: ${data.results[0].population}
+        terrain: ${data.results[0].terrain}
+    `);
 }
 
 function handleSpeciesData(data) {
-    //$('.overlay').addClass('active');
+    $('.overlay').addClass('active');
     console.log(data);
+    $('.swapi-results').html(`
+        name: ${data.results[0].name}
+        classification: ${data.results[0].classification}
+        language: ${data.results[0].language}
+        homeworld: ${data.results[0].homeworld}
+    `);
 }
 
 function handleStarshipsData(data) {
-    //$('.overlay').addClass('active');
+    $('.overlay').addClass('active');
     console.log(data);
+    $('.swapi-results').html(`
+        name: ${data.results[0].name}
+        model: ${data.results[0].model}
+        class: ${data.results[0].starship_class}
+        length: ${data.results[0].length}
+        hyperdrive rating: ${data.results[0].hyperdrive_rating}
+    `);
 }
 
 function handleVehiclesData(data) {
-    //$('.overlay').addClass('active');
+    $('.overlay').addClass('active');
     console.log(data);
+    $('.swapi-results').html(`
+        name: ${data.results[0].name}
+        model: ${data.results[0].model}
+        length: ${data.results[0].length}m
+        passengers: ${data.results[0].passengers}
+    `);
 }
 
 //Blank space code for results
